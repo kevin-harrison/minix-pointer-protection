Stichting-MINIX-Research-Foundation/minix

panic: declare as printf-style-checked
Change-Id: I3eb6b9016ef07cce941538067ea3e4f6c6a4ec44

servers/vfs/filedes.c
@@ -664,7 +664,7 @@ struct filp *f;
 	f->filp_mode = FILP_CLOSED;
 	f->filp_count = 0;
   } else if (f->filp_count < 0) {
-	panic("VFS: invalid filp count: %d ino %d/%d", f->filp_count,
+	panic("VFS: invalid filp count: %d ino %d/%ld", f->filp_count,
 	      vp->v_dev, vp->v_inode_nr);
   } else {
 	unlock_vnode(f->filp_vno);

kevin-harrison/minix-pointer-protection

	. and related fixes
Change-Id: I5131ac57dc53d8aec8d421a34c5ceea383404d7a

servers/vfs/filedes.c
@@ -673,7 +673,7 @@ struct filp *f;
 	f->filp_mode = FILP_CLOSED;
 	f->filp_count = 0;
   } else if (f->filp_count < 0) {
-	panic("VFS: invalid filp count: %d ino %d/%d", f->filp_count,
+	panic("VFS: invalid filp count: %d ino %d/%u", f->filp_count,
 	      vp->v_dev, vp->v_inode_nr);
   } else {
 	unlock_vnode(f->filp_vno);
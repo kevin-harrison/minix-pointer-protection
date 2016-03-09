kevin-harrison/minix-pointer-protection

commit c8c290bad890ba281e4cfd9da95c1dde71ef16fa

Prepare for switch to native BSD socket API
Currently, the BSD socket API is implemented in libc, translating the
API calls to character driver operations underneath.  This approach
has several issues:

- it is inefficient, as most character driver operations are specific
  to the socket type, thus requiring that each operation start by
  bruteforcing the socket protocol family and type of the given file
  descriptor using several system calls;
- it requires that libc itself be changed every time system support
  for a new protocol is added;
- various parts of the libc implementations violate the asynchronous
  signal safety POSIX requirements.

In order to resolve all these issues at once, the plan is to turn the
BSD socket calls into system calls, thus making the BSD socket API the
"native" ABI, removing the complexity from libc and instead letting
VFS deal with the socket calls.

The overall change is going to break all networking functionality. In
order to smoothen the transition, this patch introduces the fifteen
new BSD socket system calls, and makes libc try these first before
falling back on the old behavior.  For now, the VFS implementations of
the new calls fail such that libc will always use the fallback cases.
Later on, when we introduce the actual implementation of the native
BSD socket calls, all statically linked programs will automatically
use the new ABI, thus limiting actual application breakage.

In other words: by itself, this patch does nothing, except add a bit
of transitional overhead that will disappear in the future.  The
largest part of the patch is concerned with adding full support for
the new BSD socket system calls to trace(1) - this early addition has
the advantage of making system call tracing output of several socket
calls much more readable already.

Both the system call interfaces and the trace(1) support have already
been tested using code that will be committed later on.

Change-Id: I3460812be50c78be662d857f9d3d6840f3ca917f

minix/lib/libc/sys/accept.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -18,19 +19,49 @@
 #include <net/gen/udp.h>
 #include <net/gen/udp_io.h>
 
-#define DEBUG 0
-
 static int _tcp_accept(int sock, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len);
 
 static int _uds_accept(int sock, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len);
 
+/*
+ * Accept a connection on a listening socket, creating a new socket.
+ */
+static int
+__accept(int fd, struct sockaddr * __restrict address,
+	socklen_t * __restrict address_len)
+{
+	message m;
+	int r;
+
+	if (address != NULL && address_len == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sockaddr.fd = fd;
+	m.m_lc_vfs_sockaddr.addr = (vir_bytes)address;
+	m.m_lc_vfs_sockaddr.addr_len = (address != NULL) ? *address_len : 0;
+
+	if ((r = _syscall(VFS_PROC_NR, VFS_ACCEPT, &m)) < 0)
+		return -1;
+
+	if (address != NULL)
+		*address_len = m.m_vfs_lc_socklen.len;
+	return r;
+}
+
 int accept(int sock, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len)
 {
 	int r;
 	nwio_udpopt_t udpopt;
+
+	r = __accept(sock, address, address_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 	r= _tcp_accept(sock, address, address_len);
 	if (r != -1 || errno != ENOTTY)
@@ -45,19 +76,14 @@ int accept(int sock, struct sockaddr *__restrict address,
 	 * filedescriptors that do not refer to a socket.
 	 */
 	r= ioctl(sock, NWIOGUDPOPT, &udpopt);
-	if (r == 0)
-	{
+	if (r == 0 || (r == -1 && errno != ENOTTY)) {
 		/* UDP socket */
 		errno= EOPNOTSUPP;
 		return -1;
 	}
-	if (errno == ENOTTY)
-	{
-		errno= ENOTSOCK;
-		return -1;
-	}
 
-	return r;
+	errno = ENOTSOCK;
+	return -1;
 }
 
 static int _tcp_accept(int sock, struct sockaddr *__restrict address,

minix/lib/libc/sys/bind.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
 
 #include <unistd.h>
 #include <stdint.h>
@@ -33,12 +34,32 @@ static int _udp_bind(int sock, const struct sockaddr *address,
 static int _uds_bind(int sock, const struct sockaddr *address,
 	socklen_t address_len, struct sockaddr_un *uds_addr);
 
+/*
+ * Bind a socket to a local address.
+ */
+static int
+__bind(int fd, const struct sockaddr * address, socklen_t address_len)
+{
+	message m;
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sockaddr.fd = fd;
+	m.m_lc_vfs_sockaddr.addr = (vir_bytes)address;
+	m.m_lc_vfs_sockaddr.addr_len = address_len;
+
+	return _syscall(VFS_PROC_NR, VFS_BIND, &m);
+}
+
 int bind(int sock, const struct sockaddr *address, socklen_t address_len)
 {
 	int r;
 	nwio_tcpconf_t tcpconf;
 	nwio_udpopt_t udpopt;
 	struct sockaddr_un uds_addr;
+
+	r = __bind(sock, address, address_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);
 	if (r != -1 || errno != ENOTTY)
@@ -74,10 +95,7 @@ int bind(int sock, const struct sockaddr *address, socklen_t address_len)
 		return _uds_bind(sock, address, address_len, &uds_addr);
 	}
 
-#if DEBUG
-	fprintf(stderr, "bind: not implemented for fd %d\n", sock);
-#endif
-	errno= ENOSYS;
+	errno = ENOTSOCK;
 	return -1;
 }
 

minix/lib/libc/sys/connect.c
@@ -1,5 +1,7 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
+
 #include <minix/config.h>
 
 #include <errno.h>
@@ -31,12 +33,32 @@ static int _udp_connect(int sock, const struct sockaddr *address,
 static int _uds_connect(int sock, const struct sockaddr *address,
 	socklen_t address_len);
 
+/*
+ * Connect a socket to a remote address.
+ */
+static int
+__connect(int fd, const struct sockaddr * address, socklen_t address_len)
+{
+	message m;
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sockaddr.fd = fd;
+	m.m_lc_vfs_sockaddr.addr = (vir_bytes)address;
+	m.m_lc_vfs_sockaddr.addr_len = address_len;
+
+	return _syscall(VFS_PROC_NR, VFS_CONNECT, &m);
+}
+
 int connect(int sock, const struct sockaddr *address,
 	socklen_t address_len)
 {
 	int r;
 	nwio_tcpconf_t tcpconf;
 	nwio_udpopt_t udpopt;
+
+	r = __connect(sock, address, address_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);
 	if (r != -1 || errno != ENOTTY)
@@ -72,10 +94,7 @@ int connect(int sock, const struct sockaddr *address,
 		return r;
 	}
 
-#if DEBUG
-	fprintf(stderr, "connect: not implemented for fd %d\n", sock);
-#endif
-	errno= ENOSYS;
+	errno = ENOTSOCK;
 	return -1;
 }
 

minix/lib/libc/sys/getpeername.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
 
 #include <errno.h>
 #include <stdio.h>
@@ -15,8 +16,6 @@
 #include <net/gen/udp_io.h>
 #include <sys/un.h>
 
-#define DEBUG 0
-
 static int _tcp_getpeername(int sock, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len, nwio_tcpconf_t *tcpconfp);
 
@@ -26,6 +25,32 @@ static int _udp_getpeername(int sock, struct sockaddr *__restrict address,
 static int _uds_getpeername(int sock, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len, struct sockaddr_un *uds_addr);
 
+/*
+ * Get the remote address of a socket.
+ */
+static int
+__getpeername(int fd, struct sockaddr * __restrict address,
+	socklen_t * __restrict address_len)
+{
+	message m;
+
+	if (address_len == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sockaddr.fd = fd;
+	m.m_lc_vfs_sockaddr.addr = (vir_bytes)address;
+	m.m_lc_vfs_sockaddr.addr_len = *address_len;
+
+	if (_syscall(VFS_PROC_NR, VFS_GETPEERNAME, &m) < 0)
+		return -1;
+
+	*address_len = m.m_vfs_lc_socklen.len;
+	return 0;
+}
+
 int getpeername(int sock, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len)
 {
@@ -33,6 +58,10 @@ int getpeername(int sock, struct sockaddr *__restrict address,
 	nwio_tcpconf_t tcpconf;
 	nwio_udpopt_t udpopt;
 	struct sockaddr_un uds_addr;
+
+	r = __getpeername(sock, address, address_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);
 	if (r != -1 || errno != ENOTTY)
@@ -70,11 +99,7 @@ int getpeername(int sock, struct sockaddr *__restrict address,
 			&uds_addr);
 	}
 
-
-#if DEBUG
-	fprintf(stderr, "getpeername: not implemented for fd %d\n", sock);
-#endif
-	errno= ENOSYS;
+	errno = ENOTSOCK;
 	return -1;
 }
 

minix/lib/libc/sys/getsockname.c
@@ -1,13 +1,7 @@
-/*
-
-   getsockname()
-
-   from socket emulation library for Minix 2.0.x
-
-*/
-
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
+
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -22,9 +16,7 @@
 #include <net/gen/udp_io.h>
 #include <sys/un.h>
 
-/*
 #define DEBUG 0
-*/
 
 static int _tcp_getsockname(int fd, struct sockaddr *__restrict address,
    socklen_t *__restrict address_len, nwio_tcpconf_t *tcpconfp);
@@ -35,6 +27,32 @@ static int _udp_getsockname(int fd, struct sockaddr *__restrict address,
 static int _uds_getsockname(int fd, struct sockaddr *__restrict address,
    socklen_t *__restrict address_len, struct sockaddr_un *uds_addr);
 
+/*
+ * Get the local address of a socket.
+ */
+static int
+__getsockname(int fd, struct sockaddr * __restrict address,
+	socklen_t * __restrict address_len)
+{
+	message m;
+
+	if (address_len == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sockaddr.fd = fd;
+	m.m_lc_vfs_sockaddr.addr = (vir_bytes)address;
+	m.m_lc_vfs_sockaddr.addr_len = *address_len;
+
+	if (_syscall(VFS_PROC_NR, VFS_GETSOCKNAME, &m) < 0)
+		return -1;
+
+	*address_len = m.m_vfs_lc_socklen.len;
+	return 0;
+}
+
 int getsockname(int fd, struct sockaddr *__restrict address,
    socklen_t *__restrict address_len)
 {
@@ -43,7 +61,11 @@ int getsockname(int fd, struct sockaddr *__restrict address,
 	nwio_udpopt_t udpopt;
 	struct sockaddr_un uds_addr;
 
-#ifdef DEBUG
+	r = __getsockname(fd, address, address_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
+
+#if DEBUG
 	fprintf(stderr,"mnx_getsockname: ioctl fd %d.\n", fd);
 #endif
 
@@ -83,11 +105,7 @@ int getsockname(int fd, struct sockaddr *__restrict address,
 		return _uds_getsockname(fd, address, address_len, &uds_addr);
 	}
 
-#if DEBUG
-	fprintf(stderr, "getsockname: not implemented for fd %d\n", socket);
-#endif
-
-	errno= ENOSYS;
+	errno = ENOTSOCK;
 	return -1;
 }
 

minix/lib/libc/sys/getsockopt.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
 
 #include <assert.h>
 #include <errno.h>
@@ -30,6 +31,34 @@ static int _uds_getsockopt(int sock, int level, int option_name,
 static void getsockopt_copy(void *return_value, size_t return_len,
 	void *__restrict option_value, socklen_t *__restrict option_len);
 
+/*
+ * Get socket options.
+ */
+static int
+__getsockopt(int fd, int level, int option_name,
+	void * __restrict option_value, socklen_t * __restrict option_len)
+{
+	message m;
+
+	if (option_len == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sockopt.fd = fd;
+	m.m_lc_vfs_sockopt.level = level;
+	m.m_lc_vfs_sockopt.name = option_name;
+	m.m_lc_vfs_sockopt.buf = (vir_bytes)option_value;
+	m.m_lc_vfs_sockopt.len = *option_len;
+
+	if (_syscall(VFS_PROC_NR, VFS_GETSOCKOPT, &m) < 0)
+		return -1;
+
+	*option_len = m.m_vfs_lc_socklen.len;
+	return 0;
+}
+
 int getsockopt(int sock, int level, int option_name,
         void *__restrict option_value, socklen_t *__restrict option_len)
 {
@@ -37,6 +66,10 @@ int getsockopt(int sock, int level, int option_name,
 	nwio_tcpopt_t tcpopt;
 	nwio_udpopt_t udpopt;
 	struct sockaddr_un uds_addr;
+
+	r = __getsockopt(sock, level, option_name, option_value, option_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 	r= ioctl(sock, NWIOGTCPOPT, &tcpopt);
 	if (r != -1 || errno != ENOTTY)
@@ -74,11 +107,7 @@ int getsockopt(int sock, int level, int option_name,
 			option_value, option_len);
 	}
 
-
-#if DEBUG
-	fprintf(stderr, "getsockopt: not implemented for fd %d\n", sock);
-#endif
-	errno= ENOTSOCK;
+	errno = ENOTSOCK;
 	return -1;
 }
 

minix/lib/libc/sys/listen.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
 
 #include <errno.h>
 #include <stdio.h>
@@ -14,11 +15,28 @@
 #include <net/gen/udp.h>
 #include <net/gen/udp_io.h>
 
-#define DEBUG 0
+/*
+ * Put a socket in listening mode.
+ */
+static int
+__listen(int fd, int backlog)
+{
+	message m;
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_listen.fd = fd;
+	m.m_lc_vfs_listen.backlog = backlog;
+
+	return _syscall(VFS_PROC_NR, VFS_LISTEN, &m);
+}
 
 int listen(int sock, int backlog)
 {
 	int r;
+
+	r = __listen(sock, backlog);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 	r= ioctl(sock, NWIOTCPLISTENQ, &backlog);
 	if (r != -1 || errno != ENOTTY)
@@ -28,10 +46,6 @@ int listen(int sock, int backlog)
 	if (r != -1 || errno != ENOTTY)
 		return r;
 
-#if DEBUG
-	fprintf(stderr, "listen: not implemented for fd %d\n", sock);
-#endif
-	errno= ENOSYS;
+	errno = ENOTSOCK;
 	return -1;
 }
-

minix/lib/libc/sys/recvfrom.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include "namespace.h"
+#include <lib.h>
 
 #include <assert.h>
 #include <errno.h>
@@ -36,6 +37,38 @@ static ssize_t _uds_recvfrom_dgram(int sock, void *__restrict buffer,
 	size_t length, int flags, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len);
 
+/*
+ * Receive a message from a socket.
+ */
+static ssize_t
+__recvfrom(int fd, void * __restrict buffer, size_t length, int flags,
+	struct sockaddr * __restrict address,
+	socklen_t * __restrict address_len)
+{
+	message m;
+	ssize_t r;
+
+	if (address != NULL && address_len == NULL) {
+		errno = EFAULT;
+		return -1;
+	}
+
+	memset(&m, 0, sizeof(m));
+	m.m_lc_vfs_sendrecv.fd = fd;
+	m.m_lc_vfs_sendrecv.buf = (vir_bytes)buffer;
+	m.m_lc_vfs_sendrecv.len = length;
+	m.m_lc_vfs_sendrecv.flags = flags;
+	m.m_lc_vfs_sendrecv.addr = (vir_bytes)address;
+	m.m_lc_vfs_sendrecv.addr_len = (address != NULL) ? *address_len : 0;
+
+	if ((r = _syscall(VFS_PROC_NR, VFS_RECVFROM, &m)) < 0)
+		return -1;
+
+	if (address != NULL)
+		*address_len = m.m_vfs_lc_socklen.len;
+	return r;
+}
+
 ssize_t recvfrom(int sock, void *__restrict buffer, size_t length,
 	int flags, struct sockaddr *__restrict address,
 	socklen_t *__restrict address_len)
@@ -46,6 +79,10 @@ ssize_t recvfrom(int sock, void *__restrict buffer, size_t length,
 	nwio_ipopt_t ipopt;
 	struct sockaddr_un uds_addr;
 	int uds_sotype = -1;
+
+	r = __recvfrom(sock, buffer, length, flags, address, address_len);
+	if (r != -1 || errno != ENOTSOCK)
+		return r;
 
 #if DEBUG
 	fprintf(stderr, "recvfrom: for fd %d\n", sock);
@@ -121,12 +158,10 @@ ssize_t recvfrom(int sock, void *__restrict buffer, size_t length,
 		}
 
 		return rd;
-       }
+	}
 
-#if DEBUG
-	fprintf(stderr, "recvfrom: not implemented for fd %d\n", sock);
-#endif
-	abort();
+	errno = ENOTSOCK;
+	return -1;
 }
 
 static ssize_t _tcp_recvfrom(int sock, void *__restrict buffer, size_t length,

commit 063c4703734c069e22fc44bce7ca026b545f43bc

libc: fix local from-source upgrades
Commit git-c8c290b inadvertently broke local MINIX3-on-MINIX3 builds,
since its libc changes relied on VFS being upgraded already as well.
As a result, after installing the new libc, networking ceased to work,
leading to curl(1) failing later on in the build process.  This patch
introduces transitional code that is necessary for the build process
to complete, after which it is obsolete again.

Change-Id: I93bf29c01d228e3d7efc7b01befeff682954f54d

minix/lib/libc/sys/accept.c
@@ -60,7 +60,7 @@ int accept(int sock, struct sockaddr *__restrict address,
 	nwio_udpopt_t udpopt;
 
 	r = __accept(sock, address, address_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= _tcp_accept(sock, address, address_len);

minix/lib/libc/sys/bind.c
@@ -58,7 +58,7 @@ int bind(int sock, const struct sockaddr *address, socklen_t address_len)
 	struct sockaddr_un uds_addr;
 
 	r = __bind(sock, address, address_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);

minix/lib/libc/sys/connect.c
@@ -57,7 +57,7 @@ int connect(int sock, const struct sockaddr *address,
 	nwio_udpopt_t udpopt;
 
 	r = __connect(sock, address, address_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);

minix/lib/libc/sys/getpeername.c
@@ -60,7 +60,7 @@ int getpeername(int sock, struct sockaddr *__restrict address,
 	struct sockaddr_un uds_addr;
 
 	r = __getpeername(sock, address, address_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);

minix/lib/libc/sys/getsockname.c
@@ -62,7 +62,7 @@ int getsockname(int fd, struct sockaddr *__restrict address,
 	struct sockaddr_un uds_addr;
 
 	r = __getsockname(fd, address, address_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 #if DEBUG

minix/lib/libc/sys/getsockopt.c
@@ -68,7 +68,7 @@ int getsockopt(int sock, int level, int option_name,
 	struct sockaddr_un uds_addr;
 
 	r = __getsockopt(sock, level, option_name, option_value, option_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPOPT, &tcpopt);

minix/lib/libc/sys/listen.c
@@ -35,7 +35,7 @@ int listen(int sock, int backlog)
 	int r;
 
 	r = __listen(sock, backlog);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOTCPLISTENQ, &backlog);

minix/lib/libc/sys/recvfrom.c
@@ -81,7 +81,7 @@ ssize_t recvfrom(int sock, void *__restrict buffer, size_t length,
 	int uds_sotype = -1;
 
 	r = __recvfrom(sock, buffer, length, flags, address, address_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 #if DEBUG

minix/lib/libc/sys/recvmsg.c
@@ -90,7 +90,7 @@ ssize_t recvmsg(int sock, struct msghdr *msg, int flags)
 	int uds_sotype;
 
 	r = __recvmsg(sock, msg, flags);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	if (msg == NULL) {

minix/lib/libc/sys/sendmsg.c
@@ -88,7 +88,7 @@ ssize_t sendmsg(int sock, const struct msghdr *msg, int flags)
 	int uds_sotype;
 
 	r = __sendmsg(sock, msg, flags);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	if (msg == NULL) {

minix/lib/libc/sys/sendto.c
@@ -63,7 +63,7 @@ ssize_t sendto(int sock, const void *message, size_t length, int flags,
 	int uds_sotype = -1;
 
 	r = __sendto(sock, message, length, flags, dest_addr, dest_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPOPT, &tcpopt);

minix/lib/libc/sys/setsockopt.c
@@ -56,7 +56,7 @@ int setsockopt(int sock, int level, int option_name,
 	struct sockaddr_un uds_addr;
 
 	r = __setsockopt(sock, level, option_name, option_value, option_len);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPOPT, &tcpopt);

minix/lib/libc/sys/shutdown.c
@@ -40,7 +40,7 @@ int shutdown(int sock, int how)
 	nwio_tcpconf_t tcpconf;
 
 	r = __shutdown(sock, how);
-	if (r != -1 || errno != ENOTSOCK)
+	if (r != -1 || (errno != ENOTSOCK && errno != ENOSYS))
 		return r;
 
 	r= ioctl(sock, NWIOGTCPCONF, &tcpconf);

minix/lib/libc/sys/socket.c
@@ -60,7 +60,7 @@ int socket(int domain, int type, int protocol)
 	int r, sock_type;
 
 	r = __socket(domain, type, protocol);
-	if (r != -1 || errno != EAFNOSUPPORT)
+	if (r != -1 || (errno != EAFNOSUPPORT && errno != ENOSYS))
 		return r;
 
 	sock_type = type & ~SOCK_FLAGS_MASK;

minix/lib/libc/sys/socketpair.c
@@ -45,7 +45,7 @@ socketpair(int domain, int type, int protocol, int sv[2])
 	int r;
 
 	r = __socketpair(domain, type, protocol, sv);
-	if (r != -1 || errno != EAFNOSUPPORT)
+	if (r != -1 || (errno != EAFNOSUPPORT && errno != ENOSYS))
 		return r;
 
 #if DEBUG
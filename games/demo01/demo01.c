2b881e40184b5d830e6b0b07034d1e196fff37bd
Roberto Guanciale <robertog@kth.se>
2020-11-11 09:26:14 +0100

    Example

--- games/demo01/demo01.c
+++ games/demo01/demo01.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+
+char pwd[16] = "admin=admin";
+char buffer[16];
+int * x;
+
+void print_pwd(void);
+
+void print_pwd(void) {
+  printf("My password is %s\n", pwd);
+}
 
 int main(int argc, char *argv[])
 {
-  int x = 0;
-  int * y = malloc(sizeof(int));
-  printf("Hello I'm demo 01\n");
-  printf("Heap address  %p\n", y);
-  printf("Stack address %p\n", &x);
-  printf("Code address %p\n", &main);
+  int pwd_diff = strcmp(argv[1], pwd);
+  x = malloc(sizeof(int) * 1);
+  printf("addresses %p %p %p %p\n", &pwd_diff, &x, x, buffer);
 
+  strcpy(buffer, argv[2]);
+  *x = atoi(argv[3]);
+
+  if (pwd_diff != 0)
+    return 1;
+
+  print_pwd();
   return 0;
 }

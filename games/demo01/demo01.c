124125036f5012862d86b5e5197e3191d62dd6ec
Roberto Guanciale <robertog@kth.se>
2021-11-04 13:10:37 +0100

    experiments

--- games/demo01/demo01.c
+++ games/demo01/demo01.c
@@ -2,24 +2,28 @@
 #include <string.h>
 #include <stdlib.h>
 
-char pwd[16] = "admin=admin";
-char buffer[16];
-int * x;
+typedef struct _state {
+  char buffer[16];
+  int * x;
+} State;
+
+State state;
 
 void print_pwd(void);
 
 void print_pwd(void) {
-  printf("My password is %s\n", pwd);
+  printf("My password is admin\n");
 }
 
 int main(int argc, char *argv[])
 {
-  int pwd_diff = strcmp(argv[1], pwd);
-  x = malloc(sizeof(int) * 1);
-  printf("addresses %p %p %p %p\n", &pwd_diff, &x, x, buffer);
+  int pwd_diff = strcmp(argv[1], "admin");
+  state.x = malloc(sizeof(int) * 1);
+  printf("addresses %p %p %p %p \n", &pwd_diff, &(state.x), state.x, state.buffer);
 
-  strcpy(buffer, argv[2]);
-  *x = atoi(argv[3]);
+  strcpy(state.buffer, argv[2]);
+  printf("addresses %p %p %p %p \n", &pwd_diff, &(state.x), state.x, state.buffer);
+  *(state.x) = atoi(argv[3]);
 
   if (pwd_diff != 0)
     return 1;

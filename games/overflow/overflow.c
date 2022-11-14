2de356f5d4307396259a5556687a872c72f80b99
Kevin <kevin@harrisoncentral.com>
2022-11-14 13:43:44 +0100

    added overflow vulnerable program to img at /usr/games/overflow

---
+++ games/overflow/overflow.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct _state {
+  char buffer[16];
+  int * x;
+} State;
+
+State state;
+
+void print_pwd(void);
+
+void print_pwd(void) {
+  printf("My password is admin\n");
+}
+
+int main(int argc, char *argv[])
+{
+  int pwd_diff = strcmp(argv[1], "admin");
+  state.x = malloc(sizeof(int) * 1);
+  printf("addresses %p %p %p %p \n", &pwd_diff, &(state.x), state.x, state.buffer);
+
+  strcpy(state.buffer, argv[2]);
+  printf("addresses %p %p %p %p \n", &pwd_diff, &(state.x), state.x, state.buffer);
+  *(state.x) = atoi(argv[3]);
+
+  if (pwd_diff != 0)
+    return 1;
+
+  print_pwd();
+  return 0;
+}

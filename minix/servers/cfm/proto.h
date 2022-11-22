69ba829b81de3040d8d0c116d57c859ab5c64c8a
Christopher Gauffin <christopher.gauffin@gmail.com>
2022-11-22 12:46:58 +0100

    pre-rebase aslr-lab

---
+++ minix/servers/cfm/proto.h
@@ -0,0 +1,10 @@
+#ifndef _CFM_PROTO_H
+#define _CFM_PROTO_H
+
+int main(int argc, char **argv);
+
+int do_verify_hash(message *m_ptr);
+int sef_cb_init_fresh(int type, sef_init_info_t *t);
+
+
+#endif

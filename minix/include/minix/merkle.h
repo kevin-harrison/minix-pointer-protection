ff31a8a34c62ae8bf81fff37ad6106ecc6d78079
kevin <kevinha@kth.se>
2022-12-08 23:25:20 +0100

    Added merkle.c and md5.c as system libraries

---
+++ minix/include/minix/merkle.h
@@ -0,0 +1,31 @@
+#ifndef MERKLE_H
+#define MERKLE_H
+
+
+typedef struct merkle_tree_node merkle_tree_node;
+struct merkle_tree_node { 
+    char *hash;
+    void **from_address;    // the address of the protected pointer
+    void *to_address;       // the value of the protected pointer
+    merkle_tree_node *left;
+    merkle_tree_node *right;
+};
+
+//typedef char* (*Hash_Function) (unsigned char, unsigned int, unsigned char);
+//typedef char* (*Hash_Function) (merkle_tree_node*);
+
+typedef struct {
+    merkle_tree_node *root;
+} merkle_tree;
+
+
+void addPointer(void **from);
+void removePointer(void **from);
+int validatePointer(void **from);
+
+// Macros to inject into user programs
+#define STORE_POINTER(ptr) addPointer((void**)&(ptr));
+#define REMOVE_POINTER(ptr) removePointer((void**)&(ptr));
+#define VALIDATE_POINTER(ptr) if(validatePointer((void**)&(ptr)) != 1) { printf("Tried to access unverified pointer. Killing process.\n"); exit(EXIT_FAILURE);}
+ 
+#endif 

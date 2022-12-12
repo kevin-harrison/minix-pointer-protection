ae96c72f59f4f51fc3833231040c80e6f069cdba
Jesper Lagnelöv <lagnelov@kth.se>
2022-12-12 10:21:48 +0100

    Hash Function in CFM server

--- minix/servers/cfm/store.c
+++ minix/servers/cfm/store.c
@@ -22,8 +22,49 @@ int do_cfm_get_hash(message *m)
   printf("do_cfm_get_hash received %c\n", data[0]);
 
   // Hash here and update data and dataLen respectively.
-  uint8_t hash[] = {'Y'};
-  unsigned int hashLen = 1;
+  uint8_t hash[16];
+  unsigned int hashLen = sizeof(hash);
+
+  aes256_context_t ctx;
+  aes256_key_t key = {
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+                0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+        };
+
+  aes256_blk_t buf = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  aes256_init(&ctx, &key);
+  //printf("Adress: %li\n", *address);
+
+  //Handle longer messages spanning multiple blocks
+  if(dataLen > 16){
+      //Calculate number of rounds
+      unsigned int rounds = dataLen/16;
+      if((dataLen % 16) != 0){
+          rounds = rounds + 1;
+      }
+
+      memcpy(&buf, data, sizeof(buf));
+      aes256_encrypt_ecb(&ctx, &buf); //Encrypt first block
+
+      //Encrypt remaining blocks
+      unsigned int counter = 16;
+      for(int i = 1; i < rounds; i++){
+          //XOR Encrypted block with next block
+          for(int j = 0; (j < 16) && (counter < dataLen); j++){
+              counter = counter + 1;
+              buf.raw[j] = buf.raw[j] ^ data[counter];
+          }
+          aes256_encrypt_ecb(&ctx, &buf);//Encrypt block again
+      }
+  }
+  //Handle single block messages
+  else{
+      memcpy(&buf, data, dataLen);
+      aes256_encrypt_ecb(&ctx, &buf);
+  }
+  
+  //Copy final hash from buffer to hash
+  memcpy(hash, &buf, sizeof(hash));
 
   // Update the message
   memset(m, 0, sizeof(message));
